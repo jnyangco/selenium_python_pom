2795f02c39236b279cedb30739679a24e43caf7b
jeromeyangco <yangco.jerome@gmail.com>
2024-09-25 09:23:02 -0400

    Commit 09/25/2024 - moved browser setup to conftest.py

---
+++ testcases/conftest.py
@@ -0,0 +1,55 @@
+# Configuration Test -> common pytest method
+import pytest
+from selenium import webdriver
+
+@pytest.fixture()
+def set_up(): # method level setup (default scope)
+    print("\n---------- Run before method (conftest.py) ----------")  # before method
+    yield
+    print("\nRun after method (conftest.py)")  # after method
+
+
+# scope -> "module" (default), "class", "session" ... # module setup (i.e: Open browser > Quit browser)
+@pytest.fixture(scope="class")
+def onetime_setup(request, browser, os_type):
+    print("\n========== Run one time setup (conftest.py) ==========")  # before method
+    if browser.lower() == "firefox":
+        base_url = "https://opensource-demo.orangehrmlive.com"
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(5)
+        driver.get(base_url)
+        print(">>> Browser = Firefox")
+    else:
+        base_url = "https://opensource-demo.orangehrmlive.com"
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(5)
+        driver.get(base_url)
+        print(">>> Browser = Chrome")
+
+    if request.cls is not None:
+        request.cls.driver = driver  # add value to class attribute
+
+    yield driver  # yield + return driver instance for (Test Classes and Page Classes)
+
+    print("\n========== Run one time teardown (conftest.py) ==========")  # after method
+    driver.quit()
+
+
+# ==================================================================
+# actual usage
+# --envi qa1 --browser chrome --report True --headless False
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+    parser.addoption("--os_type", help="Type of operating system")
+
+
+@pytest.fixture(scope="session")
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+@pytest.fixture(scope="session")
+def os_type(request):
+    return request.config.getoption("--os_type")
+
+
--- testcases/login/TC001_login_test.py
+++ testcases/login/TC001_login_test.py
@@ -3,46 +3,37 @@ import unittest
 from selenium import webdriver
 from pages.login.login_page import LoginPage
 import pytest
+# install pytest-order
 
+@pytest.mark.usefixtures("onetime_setup", "set_up")
 class LoginTest(unittest.TestCase):
-    base_url = "https://opensource-demo.orangehrmlive.com"
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(5)
+
+    @pytest.fixture(autouse=True)
+    def class_setup(self, onetime_setup): # need to put here "onetime_setup" to access return value
+        self.lp = LoginPage(self.driver)
 
 
-    @pytest.mark.run(order=2)
+    @pytest.mark.order(2)
     def test_valid_login(self):
-        # Step 1: Go to url https://opensource-demo.orangehrmlive.com
-        self.driver.get(self.base_url)
+        # Step 1: Login using username and password
+        self.lp.login("Admin", "admin123")
 
-        # Step 2: Login using username and password
-        lp = LoginPage(self.driver)
-        lp.login("Admin", "admin123")
-
-        # Step 3: User icon should be displayed
-        result = lp.verify_login_successful()
+        # Step 2: User icon should be displayed
+        result = self.lp.verify_login_successful()
         assert result == True
 
-        # Step 4: Close the browser
         time.sleep(2)
-        self.driver.quit()
 
 
-    @pytest.mark.run(order=1)
+    @pytest.mark.order(1)
     def test_invalid_login(self):
-        # Step 1: Go to url https://opensource-demo.orangehrmlive.com
-        self.driver.get(self.base_url)
+        # Step 1: Login using username and password
+        self.lp.login("Admin", "admin1234")
 
-        # Step 2: Login - invalid password
-        lp = LoginPage(self.driver)
-        lp.login("Admin", "admin1234")
-
-        # Step 3: Verify error message "Invalid credentials" is displayed
-        result = lp.verify_login_failed()
+        # Step 2: Verify error message "Invalid credentials" is displayed
+        result = self.lp.verify_login_failed()
         assert result == "Invalid credentials"
 
-        # Step 4: Close the browser
         time.sleep(2)
-        # self.driver.quit()
 
 
